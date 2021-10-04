ce5a7051a46a2d52e77095a373685b4911aeb5f6
fred <fjricci@us.ibm.com>
2021-10-04 11:07:35 -0400

    Fix suite name in test report
    Signed-off-by: fred <fjricci@us.ibm.com>
    Signed-off-by: Aram Openden <Aram.Openden1@ibm.com>

--- test/spec/upload_test_reports.rb
+++ test/spec/upload_test_reports.rb
@@ -21,12 +21,14 @@ if ARGV[0] == 'IVT'
   logger.info("Uploading ivttest-#{time}.xml to COS")
   doc = Nokogiri::XML(File.open("#{Dir.pwd}/ivttest.xml")) { |file| file.noblanks }
   doc.search('//testsuite').attribute('name').value = "hri-flink-validation-fhir - #{ENV['BRANCH_NAME']} - IVT"
+  File.write("#{Dir.pwd}/ivttest.xml", doc)
   File.rename("#{Dir.pwd}/ivttest.xml", "#{Dir.pwd}/hri-flink-validation-fhir-ivttest-#{time}.xml")
   cos_helper.upload_object_data('wh-hri-dev1-allure-reports', "hri-flink-validation-fhir-ivttest-#{time}.xml", File.read(File.join(Dir.pwd, "hri-flink-validation-fhir-ivttest-#{time}.xml")))
 elsif ARGV[0] == 'Nightly'
   logger.info("Uploading nightlytest-#{time}.xml to COS")
   doc = Nokogiri::XML(File.open("#{Dir.pwd}/nightlytest.xml")) { |file| file.noblanks }
   doc.search('//testsuite').attribute('name').value = "hri-flink-validation-fhir - #{ENV['BRANCH_NAME']} - Nightly"
+  File.write("#{Dir.pwd}/nightlytest.xml", doc)
   File.rename("#{Dir.pwd}/nightlytest.xml", "#{Dir.pwd}/hri-flink-validation-fhir-nightlytest-#{time}.xml")
   cos_helper.upload_object_data('wh-hri-dev1-allure-reports', "hri-flink-validation-fhir-nightlytest-#{time}.xml", File.read(File.join(Dir.pwd, "hri-flink-validation-fhir-nightlytest-#{time}.xml")))
 else
