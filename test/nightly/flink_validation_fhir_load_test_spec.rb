90e4a49e7493057f67750ae3ef76423cf16f5285
Fred Ricci <60147759+FRicci618@users.noreply.github.com>
2022-04-06 09:53:50 -0400

    Use HRITestHelpers gem for the ES API
    Signed-off-by: Fred Ricci <60147759+FRicci618@users.noreply.github.com>

--- test/nightly/flink_validation_fhir_load_test_spec.rb
+++ test/nightly/flink_validation_fhir_load_test_spec.rb
@@ -16,7 +16,7 @@ describe 'Flink FHIR Validation Load Test' do
 
     @branch_name = ENV['BRANCH_NAME']
     @flink_helper = HRITestHelpers::FlinkHelper.new(ENV['FLINK_URL'])
-    @event_streams_api_helper = EventStreamsAPIHelper.new(ENV['ES_ADMIN_URL'], ENV['ES_API_KEY'])
+    @event_streams_api_helper = HRITestHelpers::EventStreamsAPIHelper.new(ENV['ES_ADMIN_URL'], ENV['ES_API_KEY'])
     @iam_token = HRITestHelpers::IAMHelper.new(ENV['IAM_CLOUD_URL']).get_access_token(ENV['CLOUD_API_KEY'])
     @appid_helper = HRITestHelpers::AppIDHelper.new(ENV['APPID_URL'], ENV['APPID_TENANT'], @iam_token, ENV['JWT_AUDIENCE_ID'])
     @flink_api_oauth_token = @appid_helper.get_access_token('hri_integration_tenant_test_data_integrator', '', ENV['APPID_FLINK_AUDIENCE'])
